3523fbc4b6080f1732aad7a67088ab3fa3dae844
Robert Sjoblom <robert.sjoblom@gmail.com>
2018-03-07 11:45:26 +0100

    config file

---
+++ js/config.js
@@ -0,0 +1,10 @@
+const config = {
+    apiKey: "AIzaSyAl4qIrAr0pjNYnvPQLp3ubQ1jRipSmYTw",
+    authDomain: "gopnik-chat.firebaseapp.com",
+    databaseURL: "https://gopnik-chat.firebaseio.com",
+    projectId: "gopnik-chat",
+    storageBucket: "gopnik-chat.appspot.com",
+    messagingSenderId: "1075818501637"
+};
+
+export { config };
