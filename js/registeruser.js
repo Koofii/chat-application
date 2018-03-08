f0bab7ea0615ee2c84e202eb073917e6bace9922
Koofii <kristoffer.frejd@gmail.com>
2018-03-08 16:41:36 +0100

    onlinestatus fixed

--- js/registeruser.js
+++ js/registeruser.js
@@ -15,26 +15,35 @@ let auth = firebase.auth();
 let provider = new firebase.auth.GithubAuthProvider();
 let userId;
 
-firebase.auth().onAuthStateChanged(function(currentUser) {
+
+
+firebase.auth().onAuthStateChanged(function (currentUser) {
     if (currentUser) {
-        $(".sign-up").hide();
-        $(".sign-in").hide();
+        // $(".sign-up").hide();
+        // $(".sign-in").hide();
         //Show chatrooms + navigation
+
     } else {
-        $(".sign-up").hide();
-        $(".sign-in").show();
+        // $(".sign-up").hide();
+        // $(".sign-in").show();
     }
 });
 
+db.ref("/users").on("child_changed", function (snapshot) {
+    console.log(snapshot.val());
+});
 // This is used to log in and verify details
 function toggleSignIn() {
     if (firebase.auth().currentUser) {
-        // [START signout]
+        const currentUser = firebase.auth().currentUser;
+        const userReference = db.ref(`/users/${currentUser.uid}`);
+        userReference.update({
+            online: false
+        });
         firebase.auth().signOut();
-        // [END signout]
     } else {
-        var email = document.getElementById('email-signin').value;
-        var password = document.getElementById('password-signin').value;
+        var email = $('#email-signin').val()
+        var password = $('#password-signin').val();
         if (email.length < 4) {
             alert('Please enter an email address.');
             return;
@@ -45,21 +54,30 @@ function toggleSignIn() {
         }
         // Sign in with email and pass.
         // [START authwithemail]
-        firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            // [START_EXCLUDE]
-            if (errorCode === 'auth/wrong-password') {
-                alert('Wrong password.');
-            } else {
-                alert(errorMessage);
-            }
-            console.log(error);
-            $(".sign-in-button").prop("disabled, false");
-            
-            // [END_EXCLUDE]
-        });
+        firebase.auth()
+            .signInWithEmailAndPassword(email, password)
+            .then(() => {
+                const currentUser = firebase.auth().currentUser;
+                const userReference = db.ref(`/users/${currentUser.uid}`);
+                userReference.update({
+                    online: true
+                });
+            })
+            .catch(function (error) {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                // [START_EXCLUDE]
+                if (errorCode === 'auth/wrong-password') {
+                    alert('Wrong password.');
+                } else {
+                    alert(errorMessage);
+                }
+                console.log(error);
+                $(".sign-in-button").prop("disabled, false");
+
+                // [END_EXCLUDE]
+            });
         // [END authwithemail]
     }
     $(".sign-in-button").prop("disabled, false");
@@ -83,7 +101,7 @@ function handleSignUp() {
     }
     // Sign in with email and pass.
     // [START createwithemail]
-    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
@@ -97,18 +115,19 @@ function handleSignUp() {
         // [END_EXCLUDE]
     });
 
-    firebase.auth().onAuthStateChanged( user => {
+    firebase.auth().onAuthStateChanged(user => {
         if (user) {
             // If user state changes and 'user' exists, check Firebase Database for user
             const userReference = db.ref(`/users/${user.uid}`);
             userReference.once('value', snapshot => {
                 if (!snapshot.val()) {
-                // User does not exist, create user entry
+                    // User does not exist, create user entry
                     userReference.set({
                         email: user.email,
                         username: username,
                         firstname: fName,
-                        lastname: lName
+                        lastname: lName,
+                        online: true
                     });
                 }
             });
@@ -116,42 +135,7 @@ function handleSignUp() {
     });
     // [END createwithemail]
 }
-/**
- * Sends an email verification to the user.
- */
-function sendEmailVerification() {
-    // [START sendemailverification]
-    firebase.auth().currentUser.sendEmailVerification().then(function() {
-        // Email Verification sent!
-        // [START_EXCLUDE]
-        alert('Email Verification Sent!');
-        // [END_EXCLUDE]
-    });
-    // [END sendemailverification]
-}
-function sendPasswordReset() {
-    var email = document.getElementById('email').value;
-    // [START sendpasswordemail]
-    firebase.auth().sendPasswordResetEmail(email).then(function() {
-        // Password Reset Email Sent!
-        // [START_EXCLUDE]
-        alert('Password Reset Email Sent!');
-        // [END_EXCLUDE]
-    }).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // [START_EXCLUDE]
-        if (errorCode == 'auth/invalid-email') {
-            alert(errorMessage);
-        } else if (errorCode == 'auth/user-not-found') {
-            alert(errorMessage);
-        }
-        console.log(error);
-        // [END_EXCLUDE]
-    });
-    // [END sendpasswordemail];
-}
+
 /**
  * initApp handles setting up UI event listeners and registering Firebase auth listeners:
  *  - firebase.auth().onAuthStateChanged: This listener is called when the user is signed in or
@@ -160,27 +144,19 @@ function sendPasswordReset() {
 function initApp() {
     // Listening for auth state changes.
     // [START authstatelistener]
-    firebase.auth().onAuthStateChanged(function(user) {
-        // [START_EXCLUDE silent]
-        $("#verify-email").prop("disabled, false");
-        // [END_EXCLUDE]
+    firebase.auth().onAuthStateChanged(function (user) {
         if (user) {
 
             // User is signed in.
             const displayName = user.displayName;
             const email = user.email;
-            const emailVerified = user.emailVerified;
-            const photoURL = user.photoURL;
-            const isAnonymous = user.isAnonymous;
             const uid = user.uid;
             const providerData = user.providerData;
             // [START_EXCLUDE]
             $('#sign-in-status').text("Signed in");
             $('#sign-in-button').text("Sign out");
             $('#account-details').text(JSON.stringify(user, null, '  '));
-            if (!emailVerified) {
-                $('#verify-email').prop("disabled, false");
-            }
+
             // [END_EXCLUDE]
         } else {
             // User is signed out.
@@ -195,14 +171,10 @@ function initApp() {
         // [END_EXCLUDE]
     });
     // [END authstatelistener]
-    
+
     $("#sign-in-button").click(toggleSignIn);
     $("#sign-up-button").click(handleSignUp);
-    $("#verify-email").click(sendEmailVerification);
-    $("#password-reset").click(sendPasswordReset);
-
-   
 }
-window.onload = function() {
+window.onload = function () {
     initApp();
 };
