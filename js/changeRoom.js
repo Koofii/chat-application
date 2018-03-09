5fc505f2523ad85b4525114d63c670309ae43b43
Robert Sjoblom <robert.sjoblom@gmail.com>
2018-03-09 13:49:14 +0100

    somewhat working. halfway point.

--- js/changeRoom.js
+++ js/changeRoom.js
@@ -3,14 +3,14 @@
 //import { config } from "./config";
 
 
-const config = {
-    apiKey: "AIzaSyAl4qIrAr0pjNYnvPQLp3ubQ1jRipSmYTw",
-    authDomain: "gopnik-chat.firebaseapp.com",
-    databaseURL: "https://gopnik-chat.firebaseio.com",
-    projectId: "gopnik-chat",
-    storageBucket: "gopnik-chat.appspot.com",
-    messagingSenderId: "1075818501637"
-};
+// const config = {
+//     apiKey: "AIzaSyAl4qIrAr0pjNYnvPQLp3ubQ1jRipSmYTw",
+//     authDomain: "gopnik-chat.firebaseapp.com",
+//     databaseURL: "https://gopnik-chat.firebaseio.com",
+//     projectId: "gopnik-chat",
+//     storageBucket: "gopnik-chat.appspot.com",
+//     messagingSenderId: "1075818501637"
+// };
 
 const ui = {
     userName: $("#userName"),
@@ -19,8 +19,8 @@ const ui = {
     output: $("#room1")
 };
 
-firebase.initializeApp(config); //eslint-disable-line
-const db = firebase.database(); //eslint-disable-line
+// firebase.initializeApp(config); //eslint-disable-line
+// const db = firebase.database(); //eslint-disable-line
 const state = {
     currentRoom: "room1"
 };
