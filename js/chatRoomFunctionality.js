9a2f38d63bd689479beeb304d98d69db5ee5f2c1
Robert Sjoblom <robert.sjoblom@gmail.com>
2018-03-13 10:47:54 +0100

    weird bug

--- js/chatRoomFunctionality.js
+++ js/chatRoomFunctionality.js
@@ -1,12 +1,8 @@
 /* global db */
-
-
-
-// firebase.initializeApp(config); //eslint-disable-line
-// const db = firebase.database(); //eslint-disable-line
-
 const state = {
-    currentRoom: "room1"
+    currentRoom: "room1",
+    msg: $("#msg"),
+    send: $("#send")
 };
 
 $("nav ul li").on("click", changeRoom);
@@ -25,16 +21,6 @@ function changeRoom() {
     }
 }
 
-function msgHandler(snapshot) {
-    //I draw messages!
-    $(`#${state.currentRoom}`).append(`<div>${snapshot.val().user} says: ${snapshot.val().message}</div>`);
-}
-
-function startChat(state) {
-    db.ref(`/chat/${state.currentRoom}`).on("child_added", msgHandler);
-}
-
-
 /*
 This is how chat messages should look
 <div>
@@ -48,5 +34,38 @@ This is how chat messages should look
     <p class="time-stamp">TIMESTAMP</p>
 </div>
 */
+function msgHandler(snapshot) {
+    $(`#${state.currentRoom}`).append(`<div>
+        <p class="user">${snapshot.val().user}<p>
+        <p class="messages">${snapshot.val().message}
+        <p class="time-stamp">${snapshot.val().time}</p>
+    </div>`);
+}
+
+function startChat(state) {
+    db.ref(`/chat/${state.currentRoom}`).on("child_added", msgHandler);
+}
+
+state.send.on("click", function(e) {
+    e.preventDefault();
+    let user = firebase.auth().currentUser;
+    let ref = db.ref(`/users/${user.uid}`);
+    ref.on("value", function(snapshot) {
+        let username = snapshot.val().username;
+        db.ref(`/chat/${state.currentRoom}`).push({
+            user: username,
+            message: state.msg.val(),
+            time: Date.now()
+        });
+    });
+    state.msg.val("");
+});
+
+state.msg.on("keydown", function(e) {
+    console.log(e.keyCode);
+    if ((e.keyCode || e.which ) === 13) {
+        $("#send").trigger("click");
+    }
+});
 
 startChat(state);
