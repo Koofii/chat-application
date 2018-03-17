b9e447495a7596f12d1f8154f45eb01016469594
Robert Sjoblom <robert.sjoblom@gmail.com>
2018-03-17 18:12:25 +0100

    structure is fucked.

--- js/onlinelist.js
+++ js/onlinelist.js
@@ -1,20 +1,16 @@
+/* global db */
 
 const userlist = $("#list-of-users ul");
 
-db.ref("/online").on("value", snapshot => {
+function updateOnline() { //eslint-disable-line
+    db.ref("/online").on("value", snapshot => {
+        const online = Object.keys(snapshot.val()).map(key => {
+            return snapshot.val()[key]["online"];
+        });
 
-    const online = Object.keys(snapshot.val()).map(key => {
-        return snapshot.val()[key]["online"];
+        userlist.html("");
+        online.forEach(val => {
+            userlist.append(`<li>${val}</li>`);
+        });
     });
-
-    userlist.html("");
-    online.forEach(val => {
-        userlist.append(`<li>${val}</li>`);
-    });
-
-    // Object.keys(snapshot.val()).forEach(val => {
-    //     console.log(snapshot.val()[val]["online"]);
-    // });
-    
-    //console.log(snapshot.val());
-})
+}
