4fc3b211b89c9fdd499fdab6b6f5b115813534db
Koofii <kristoffer.frejd@gmail.com>
2018-03-17 15:16:10 +0100

    for dist

---
+++ dist/js/chatRoomFunctionality.js
@@ -0,0 +1,60 @@
+/* global db */
+const state = {
+    currentRoom: "room1",
+    msg: $("#msg"),
+    send: $("#send"),
+    chatrooms: document.getElementById("chatrooms")
+};
+
+function changeRoom() {
+    const room = $(this).attr("data-value");
+    // empty room we're leaving
+    if (state.currentRoom !== room) {
+        $(`#${state.currentRoom}`).html("");
+        // remove event listeners
+        db.ref(`/chat/${state.currentRoom}`).off();
+        // update state
+        state.currentRoom = room;
+        // attach a listener to the new room
+        db.ref(`/chat/${room}`).on("child_added", msgHandler);
+    }
+}
+
+function msgHandler(snapshot) {
+    $(`#${state.currentRoom}`).append(`<div>
+        <p class="user">${snapshot.val().user}</p>
+        <p class="messages">${snapshot.val().message}
+        <p class="time-stamp">${snapshot.val().time}</p>
+    </div>`);
+    state.chatrooms.scrollTop = state.chatrooms.scrollHeight;
+}
+
+function startChat(state) {
+    db.ref(`/chat/${state.currentRoom}`).on("child_added", msgHandler);
+}
+
+state.send.on("click", function (e) {
+    e.preventDefault();
+    let user = firebase.auth().currentUser;
+    let ref = db.ref(`/users/${user.uid}`);
+    ref.on("value", function (snapshot) {
+        let username = snapshot.val().username;
+        let time = new Date();
+        
+        db.ref(`/chat/${state.currentRoom}`).push({
+            user: username,
+            message: state.msg.val().replace(/</g, "") || "I am poor gopnik I cannot afford send real text",
+            //ninja lvl: 1,000
+            time: time.toISOString().split("T").join(" ").slice(0, 19)
+        });
+        state.msg.val("");
+    });
+});
+
+state.msg.on("keydown", function (e) {
+    if ((e.keyCode || e.which) === 13) {
+        $("#send").trigger("click");
+    }
+});
+$("nav ul li").on("click", changeRoom);
+startChat(state);
---
+++ dist/js/registeruser.js
@@ -0,0 +1,179 @@
+/* global firebase */
+const config = {
+    apiKey: "AIzaSyAl4qIrAr0pjNYnvPQLp3ubQ1jRipSmYTw",
+    authDomain: "gopnik-chat.firebaseapp.com",
+    databaseURL: "https://gopnik-chat.firebaseio.com",
+    projectId: "gopnik-chat",
+    storageBucket: "gopnik-chat.appspot.com",
+    messagingSenderId: "1075818501637"
+};
+firebase.initializeApp(config);
+$("#reg-form").hide();
+
+$("#become-gopnik p span").click(function () {
+    $("#sign-in-view").hide();
+    $("#reg-form").fadeIn();
+});
+$("#cancel-reg p").click(function () {
+    $("#sign-in-view").fadeIn();
+    $("#reg-form").hide();
+});
+// Vars for Firebase
+let db = firebase.database();
+// let auth = firebase.auth();
+
+function removeUserFromOnlineList() {
+    const currentUser = firebase.auth().currentUser;
+    db.ref("/online/").child(`${currentUser.uid}`).remove();
+    $("#user").remove();
+    return null;
+}
+
+// This is used to log in and verify details
+function toggleSignIn() {
+    if (firebase.auth().currentUser) {
+
+        removeUserFromOnlineList();
+        firebase.auth().signOut();
+    } else {
+        let email = $("#email-signin").val()
+        let password = $("#password-signin").val();
+        if (email.indexOf("@") === -1) {
+            $("#email-signin").prop('class', 'invalid');
+            return;
+        }
+        if (password.length < 4) {
+            $("#password-signin").prop('class', 'invalid');
+            alert("Please enter a password.");
+            return;
+        }
+
+        // [START authwithemail]
+        firebase.auth()
+            .signInWithEmailAndPassword(email, password)
+            .then(() => {
+                const currentUser = firebase.auth().currentUser;
+
+                db.ref(`/users/${currentUser.uid}`).once("value", snapshot => {
+                    db.ref(`/online/${currentUser.uid}`).set({ online: snapshot.val().username });
+                });
+            })
+            .catch(function (error) {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                // [START_EXCLUDE]
+                if (errorCode === "auth/wrong-password") {
+                    alert("Wrong password.");
+                } else {
+                    alert(errorMessage);
+                }
+                console.log(error);
+                $(".sign-in-button").prop("disabled, false");
+
+                // [END_EXCLUDE]
+            });
+        // [END authwithemail]
+    }
+    $(".sign-in-button").prop("disabled, false");
+}
+/**
+ * Handles the sign up button press.
+ */
+function handleSignUp() {
+    var email = $("#email").val();
+    var password = $("#password").val();
+    var username = $("#username").val();
+    var fName = $("#fname").val();
+    var lName = $("#lname").val();
+
+    //sign up validation email
+    if (email.length < 4) {
+        alert("Please enter an email address.");
+        return;
+    }
+    if (password.length < 4) {
+        alert("Please enter a password.");
+        return;
+    }
+    // Sign in with email and pass.
+    // [START createwithemail]
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // [START_EXCLUDE]
+        //validation password sign up
+        if (errorCode == "auth/weak-password") {
+            alert("The password is too weak.");
+        } else {
+            alert(errorMessage);
+        }
+        console.log(error);
+        // [END_EXCLUDE]
+    });
+
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            // If user state changes and "user" exists, check Firebase Database for user
+            const userReference = db.ref(`/users/${user.uid}`);
+            userReference.once("value", snapshot => {
+                if (!snapshot.val()) {
+                    // User does not exist, create user entry
+                    userReference.set({
+                        email: user.email,
+                        username: username,
+                        firstname: fName,
+                        lastname: lName,
+                    });
+                }
+            });
+        }
+    });
+    // [END createwithemail]
+}
+
+function initApp() {
+    // Listening for auth state changes.
+
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            $("#rooms").fadeIn();
+            $("#chatrooms").fadeIn();
+            $("#list-of-users").fadeIn();
+            $("#input-box").fadeIn();
+            $("#div-button").fadeIn();
+            $("#sign-in-view").hide();
+
+            db.ref(`/users/${user.uid}`).on("value", snapshot => {
+                const username = snapshot.val().username;
+                $("body").append(`<div id="user" style="position: absolute; top: 50px; left: 80%;">${username}</div>`)
+            });
+
+
+
+
+        } else {
+            $("#rooms").hide();
+            $("#chatrooms").hide();
+            $("#list-of-users").hide();
+            $("#input-box").hide();
+            $("#div-button").hide();
+            $("#sign-in-view").show();
+
+        }
+    });
+    // [END authstatelistener]
+    $("#sign-in-button").click(toggleSignIn);
+    $("#sign-up-button").click(handleSignUp);
+    $("#sign-out-button").click(toggleSignIn);
+}
+
+window.onload = function () {
+    initApp();
+};
+
+
+$(window).on("beforeunload", function () {
+    removeUserFromOnlineList();
+});
